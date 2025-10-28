bbfd7c38e0bd25230b8946320c1b2faa4f8e30e4
GabrielCosta4 <gabriel_f_da-costa@estudante.sc.senai.br>
2025-10-28 16:21:01 -0300

    database pronta

--- projeto/db_config.py
+++ projeto/db_config.py
@@ -5,21 +5,23 @@ import os
 DB_HOST = 'localhost'
 DB_USER = 'root'
 DB_PASS = os.environ.get('DB_PASSWORD', 'root')
-DB_NAME = 'mapainterativo'
 
 def conectar_bd():
+    """
+    Função para conectar ao SERVIDOR MySQL.
+    (Sem especificar um banco de dados inicial)
+    """
     conexao = None
     try:
         conexao = mysql.connector.connect(
             host=DB_HOST,
             user=DB_USER,
             password=DB_PASS,
-            database=DB_NAME,
             port=3307
         )
         
         if conexao.is_connected():
-            print("Conexão ao MySQL bem-sucedida!")
+            print("Conexão ao SERVIDOR MySQL bem-sucedida!")
             return conexao
 
     except Error as e:
